a2c3aeb73d9b48767518e48017ce49b46eb6e6a5
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-03 04:50:48 -0300

    using @Query for custom JPQL queries

--- src/main/java/io/github/jonasvlima/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/jonasvlima/libraryapi/repository/LivroRepository.java
@@ -3,11 +3,16 @@ package io.github.jonasvlima.libraryapi.repository;
 import io.github.jonasvlima.libraryapi.model.Autor;
 import io.github.jonasvlima.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+
+/**
+ * @see LivroRepositoryTest
+ */
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
@@ -30,4 +35,28 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     // select * from livro where data_publicacao between ? and ?
     List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
 
+    // JPQL -> referencia as entidades e as propriedades
+    // select l.* from livro as l order by l.titulo, l.preco
+    @Query(" select l from Livro as l order by l.titulo, l.preco ")
+    List<Livro> listarTodosOrdenadoPorTituloAndPreco();
+
+    /**
+     * select a.*
+     * from livro l
+     * join autor a on a.id = l.id_autor
+     */
+    @Query(" select a from Livro l join l.autor a ")
+    List<Autor> listarAutoresDosLivros();
+
+    // select distinct l.* from l
+    @Query(" select distinct l.titulo from Livro l ")
+    List<String> listarNomesDiferentesLivros();
+
+    @Query("""
+            select l.genero
+            from Livro l
+            join l.autor a
+            where a.nacionalidade = 'Brasileira'
+            """)
+    List<String> listarGenerosAutoresBrasileiros();
 }
--- src/test/java/io/github/jonasvlima/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/io/github/jonasvlima/libraryapi/repository/LivroRepositoryTest.java
@@ -136,4 +136,28 @@ class LivroRepositoryTest {
         List<Livro> lista = repository.findByTituloAndPreco(tituloPesquisa, preco);
         lista.forEach(System.out::println);
     }
+
+    @Test
+    void listarLivrosComQueryJPQL(){
+        var resultado = repository.listarTodosOrdenadoPorTituloAndPreco();
+        resultado.forEach(System.out::println);
+    }
+
+    @Test
+    void listarAutoresDosLivros(){
+        var resultado = repository.listarAutoresDosLivros();
+        resultado.forEach(System.out::println);
+    }
+
+    @Test
+    void listarTitulosNaoRepetidosDosLivros(){
+        var resultado = repository.listarNomesDiferentesLivros();
+        resultado.forEach(System.out::println);
+    }
+
+    @Test
+    void listarGenerosDeLivrosAutoresBrasileiros(){
+        var resultado = repository.listarGenerosAutoresBrasileiros();
+        resultado.forEach(System.out::println);
+    }
 }
