47bc640f5bd37753d279848735453c6cac14c181
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-05 21:32:34 -0300

    Retrieve Author details

--- src/main/java/io/github/jonasvlima/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/jonasvlima/libraryapi/controller/AutorController.java
@@ -3,15 +3,13 @@ package io.github.jonasvlima.libraryapi.controller;
 import io.github.jonasvlima.libraryapi.controller.dto.AutorDTO;
 import io.github.jonasvlima.libraryapi.model.Autor;
 import io.github.jonasvlima.libraryapi.service.AutorService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("autores")
@@ -38,5 +36,23 @@ public class AutorController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.obterPorId(idAutor);
+        if (autorOptional.isPresent()){
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(
+                    autor.getId(),
+                    autor.getNome(),
+                    autor.getDataNascimento(),
+                    autor.getNacionalidade());
+
+            return ResponseEntity.ok(dto);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/io/github/jonasvlima/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/jonasvlima/libraryapi/controller/dto/AutorDTO.java
@@ -3,8 +3,10 @@ package io.github.jonasvlima.libraryapi.controller.dto;
 import io.github.jonasvlima.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record AutorDTO(
+        UUID id,
         String nome,
         LocalDate dataNascimento,
         String nacionalidade) {
