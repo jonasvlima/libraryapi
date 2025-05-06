2a45c86de4039559bc5b88c4e96d798c0e877505
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-05 21:00:21 -0300

    Add JPA auditing to entities

--- src/main/java/io/github/jonasvlima/libraryapi/Application.java
+++ src/main/java/io/github/jonasvlima/libraryapi/Application.java
@@ -3,9 +3,11 @@ package io.github.jonasvlima.libraryapi;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
