4b8ab137917d5cb4efa768c51ffb696a47f73d7f
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-09 03:18:26 -0300

    feat: add author search with filters

--- src/main/java/io/github/jonasvlima/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/jonasvlima/libraryapi/repository/AutorRepository.java
@@ -3,7 +3,13 @@ package io.github.jonasvlima.libraryapi.repository;
 import io.github.jonasvlima.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
 }
