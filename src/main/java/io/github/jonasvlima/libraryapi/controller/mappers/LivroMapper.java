9fe7c6b746bdae8aaa7ba52ecd411af003815c84
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-22 17:53:35 -0300

    feat: add book details mapping using composed mapper

--- src/main/java/io/github/jonasvlima/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/jonasvlima/libraryapi/controller/mappers/LivroMapper.java
@@ -1,13 +1,14 @@
 package io.github.jonasvlima.libraryapi.controller.mappers;
 
 import io.github.jonasvlima.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.jonasvlima.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.jonasvlima.libraryapi.model.Livro;
 import io.github.jonasvlima.libraryapi.repository.AutorRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = AutorMapper.class)
 public abstract class LivroMapper {
 
     @Autowired
@@ -15,4 +16,6 @@ public abstract class LivroMapper {
 
     @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
     public abstract Livro toEntity(CadastroLivroDTO dto);
+
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
 }
