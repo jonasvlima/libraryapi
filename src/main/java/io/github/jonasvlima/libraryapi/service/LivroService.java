286ab9e3fcd71db409d671ea58a4ac3658c40924
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-22 18:16:11 -0300

    docs: add explanation about Spring Data JPA Specification

--- src/main/java/io/github/jonasvlima/libraryapi/service/LivroService.java
+++ src/main/java/io/github/jonasvlima/libraryapi/service/LivroService.java
@@ -1,10 +1,13 @@
 package io.github.jonasvlima.libraryapi.service;
 
+import io.github.jonasvlima.libraryapi.model.GeneroLivro;
 import io.github.jonasvlima.libraryapi.model.Livro;
 import io.github.jonasvlima.libraryapi.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,5 +27,12 @@ public class LivroService {
 
     public void deletar(Livro livro){
         livroRepository.delete(livro);
+    }
+
+    public List<Livro> pesquisa(
+            String isbn, String nomeAutor, GeneroLivro genero, Integer anoPublicacao){
+
+        Specification<Livro> specs = null;
+        return livroRepository.findAll(specs);
     }
 }
