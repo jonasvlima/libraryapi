e61ae6b3ee96702b56e00253cc24663562c27557
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-25 18:57:14 -0300

    feat: create custom UserDetailsService

--- src/main/java/io/github/jonasvlima/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/jonasvlima/libraryapi/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.jonasvlima.libraryapi.config;
 
+import io.github.jonasvlima.libraryapi.security.CustomUserDetailsService;
+import io.github.jonasvlima.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -44,20 +46,22 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+    public UserDetailsService userDetailsService(UsuarioService usuarioService){
 
-        UserDetails user1 = User.builder()
-                .username("usuario")
-                .password(encoder.encode("123"))
-                .roles("USER")
-                .build();
+//        UserDetails user1 = User.builder()
+//                .username("usuario")
+//                .password(encoder.encode("123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("admin")
+//                .password(encoder.encode("321"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
 
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password(encoder.encode("321"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+        return new CustomUserDetailsService(usuarioService);
     }
 }
---
+++ src/main/java/io/github/jonasvlima/libraryapi/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package io.github.jonasvlima.libraryapi.security;
+
+import io.github.jonasvlima.libraryapi.model.Usuario;
+import io.github.jonasvlima.libraryapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+
+        Usuario usuario = service.obterPorLogin(login);
+
+        if (usuario == null){
+            throw new UsernameNotFoundException("Usuário não encontrado!");
+        }
+
+        return User.builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(usuario.getRoles().toArray(new String[usuario.getRoles().size()]))
+                .build();
+    }
+}
