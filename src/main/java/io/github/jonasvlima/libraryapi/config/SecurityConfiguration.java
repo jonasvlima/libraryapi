a2af7b2f25ed23bc673ef7cd9aa518c1e13bcb92
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-28 00:38:49 -0300

    Implementing Google authentication customization logic

--- src/main/java/io/github/jonasvlima/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/jonasvlima/libraryapi/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package io.github.jonasvlima.libraryapi.config;
 
 import io.github.jonasvlima.libraryapi.security.CustomUserDetailsService;
+import io.github.jonasvlima.libraryapi.security.LoginSocialSuccessHandler;
 import io.github.jonasvlima.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +23,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http, LoginSocialSuccessHandler successHandler) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
 //                .formLogin(configurer -> {
@@ -36,7 +38,9 @@ public class SecurityConfiguration {
 
                     authorize.anyRequest().authenticated();
                 })
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth2 -> {
+                    oauth2.successHandler(successHandler);
+                })
                 .build();
     }
 
