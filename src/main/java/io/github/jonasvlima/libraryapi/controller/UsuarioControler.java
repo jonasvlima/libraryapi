bd0a1e2366a4b4a1a714756fc36e74c54cd0b196
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-05-28 00:16:30 -0300

    Add field to save email in user registration

--- src/main/java/io/github/jonasvlima/libraryapi/controller/UsuarioControler.java
+++ src/main/java/io/github/jonasvlima/libraryapi/controller/UsuarioControler.java
@@ -3,6 +3,7 @@ package io.github.jonasvlima.libraryapi.controller;
 import io.github.jonasvlima.libraryapi.controller.dto.UsuarioDTO;
 import io.github.jonasvlima.libraryapi.controller.mappers.UsuarioMapper;
 import io.github.jonasvlima.libraryapi.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class UsuarioControler {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void salvar(@RequestBody UsuarioDTO dto){
+    public void salvar(@RequestBody @Valid UsuarioDTO dto){
         var usuario = mapper.toEntity(dto);
         service.salvar(usuario);
     }
