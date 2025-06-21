eff6a80d13f3b07bf2b67faf2be1638b84b25d38
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-06-21 20:20:22 -0300

    feat: create custom RegisteredClientsRepository

---
+++ src/main/java/io/github/jonasvlima/libraryapi/security/CustomRegisterClientRepository.java
@@ -0,0 +1,46 @@
+package io.github.jonasvlima.libraryapi.security;
+
+import io.github.jonasvlima.libraryapi.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomRegisterClientRepository implements RegisteredClientRepository {
+
+    private final ClientService cLientService;
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        var client = cLientService.obterPorClientId(clientId);
+
+        if (client == null){
+            return null;
+        }
+
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri(client.getRedirectUri())
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+//                .tokenSettings()
+//                .clientSettings()
+                .build();
+    }
+}
