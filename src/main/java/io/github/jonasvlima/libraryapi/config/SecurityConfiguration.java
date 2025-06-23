536f2df31970d2e4a5373243918a26198683fe35
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-06-23 19:31:41 -0300

    feat(auth): configuring Resource Server

--- src/main/java/io/github/jonasvlima/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/jonasvlima/libraryapi/config/SecurityConfiguration.java
@@ -1,8 +1,6 @@
 package io.github.jonasvlima.libraryapi.config;
 
-import io.github.jonasvlima.libraryapi.security.CustomUserDetailsService;
 import io.github.jonasvlima.libraryapi.security.LoginSocialSuccessHandler;
-import io.github.jonasvlima.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,9 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -42,11 +39,24 @@ public class SecurityConfiguration {
                             .loginPage("/login")
                             .successHandler(successHandler);
                 })
+                .oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()))
                 .build();
     }
-
+//  Configura o prefixo Role
     @Bean
     public GrantedAuthorityDefaults grantedAuthorityDefaults(){
         return new GrantedAuthorityDefaults("");
+    }
+
+//    Configura no TOKEN JWT, o prefixo Scope
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        var authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthorityPrefix("");
+
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return converter;
     }
 }
